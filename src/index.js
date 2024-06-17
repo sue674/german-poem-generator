a0d9d52b0ddb0daaa8c131b85d1d54589f165888
Sue <gusu674@mahalo-mail.com>
2024-06-17 19:22:23 +0200

    included AI response for poem

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 
-function generatePoem(event){
-   
-event.preventDefault();
+function displayPoem(response){
 new Typewriter('#poem', {
-  strings: "The poem will go header",
+  strings: response.data.answer,
   autoStart: true,
 });
+}
+
+function generatePoem(event){
+    event.preventDefault();
+   //build api url
+   let instructions = document.querySelector("#user-instruction")
+   let apiKey = "feaeta6473b4b23fd60370ob33dc4c40";
+let prompt = `Write a short poem of 4 lines in German about  ${instructions}`
+let context = "You are a romantic poet and write short poems about the topic given to you. Put them in 4 lines and separate them with a <br /> element. Sign it with 'SheCodes AI' at the bottom in a <strong> element. "
+   let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+   //get answer from api using axios
+   axios.get(apiUrl).then(displayPoem);
+   //display poem insted of dummy
+
+
 }
    
 
