bb70ad5cbf9515c84a1c7a35444235cff6360249
Sue <gusu674@mahalo-mail.com>
2024-06-18 10:12:55 +0200

    improved UX

--- src/index.js
+++ src/index.js
@@ -8,16 +8,21 @@ new Typewriter('#poem', {
 
 function generatePoem(event){
     event.preventDefault();
-   //build api url
+   
    let instructions = document.querySelector("#user-instruction")
    let apiKey = "feaeta6473b4b23fd60370ob33dc4c40";
-let prompt = `Write a short poem of 4 lines in German about  ${instructions}`
+let prompt = `Write a short poem of 4 lines in German about  ${instructions.value}`
 let context = "You are a romantic poet and write short poems about the topic given to you. Put them in 4 lines and separate them with a <br /> element. Sign it with 'SheCodes AI' at the bottom in a <strong> element. "
    let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
-   //get answer from api using axios
+  
    axios.get(apiUrl).then(displayPoem);
-   //display poem insted of dummy
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML =  `<div class="generating"> Generating a German poem about ${instructions.value} for you </div>`
 
+console.log("Generating poem");
+console.log(`prompt: ${prompt}`);
+console.log(`context: ${context}`);
 
 }
    
